9d3e3200b9f9860b265ee6eff3ce4c3cc426db55
mariotosdig <mariotosdig2@gmail.com>
2019-12-02 15:55:18 -0400

    owo

--- Assignment4.py
+++ Assignment4.py
@@ -7,10 +7,10 @@ inventory = ["apple"]
 cash_money = 0
 gameover = False
 
-def input_request(options, prompt, inventory):
+def input_request(options, prompt, inventory, current_room):
     x = None
-
-    dictionary_of_things_in_room={"entrance": None, "street": "jackhammer", "outside bank": None, "vents": "coin", "matrix": None, "bank lobby":None, "railroad":"crowbar" }
+    things_in_room = ""
+    room_dict = {"entrance": None, "city": "jackhammer", "outside bank": None, "vents": "coin", "matrix": None, "bank lobby":None, "railroad":"crowbar" }
 
     while x not in options:
         x = input(prompt).lower()
@@ -25,7 +25,15 @@ def input_request(options, prompt, inventory):
             x = None
 
         elif x == "r":
+            print("=============================================")
+            print("Current room: = {}. ".format(current_room))
+            if things_in_room == None:
+                 things_in_room = "nothing"
+            else:
+                things_in_room = room_dict[current_room]
 
+            print("This room contains: {}. ".format(things_in_room))
+            print("=============================================")
 
         elif x == "d":
             x = input("Which item would you like to drop? ").lower()
@@ -46,6 +54,7 @@ print("You can also chose to drop one of your items with an input of 'd'.")
 input("Press the Enter key to start. ")
 print('\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n')
 
+current_room = "entrance"
 print("You appear from the abyss. ")
 time.sleep(1)
 print("A magical wizard appears in front of you. ")
@@ -55,15 +64,16 @@ time.sleep(2)
 print("Wizard: choose your path wisely young one as it will determine your fate. ")
 time.sleep(2)
 
-x = input_request(["w"], "Enter 'w' to walk forward.", inventory)
+x = input_request(["w"], "Enter 'w' to walk forward. ", inventory, current_room)
 
 print('\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n')
+current_room = "city"
 print ("You appear to be in a metropoliton city.")
 time.sleep(2)
 print("You see a jackhammer in front of you. ")
 time.sleep(1)
 
-x = input_request(["y","n"], "Would you like to pick it up? [y/n] ", inventory)
+x = input_request(["y","n"], "Would you like to pick it up? [y/n] ", inventory, current_room)
 
 if x == "y":
     print("You pick up the jackhammer. ")
@@ -75,16 +85,17 @@ elif x == "n":
 
 print("Would you like to go to the bank or the railroad? [b/r]")
 
-x = input_request(["b","r"], "Enter 'b' for bank or 'r' for railroad. ", inventory)
+x = input_request(["b","r"], "Enter 'b' for bank or 'r' for railroad. ", inventory, current_room)
 
 while gameover == False:
     if x == "r":
         print('\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n')
+        current_room = "railroad"
         print("You have arrived at the railroad")
         time.sleep(1)
         print("You see a crowbar on the ground.")
 
-        x = input_request(["y","n"], "Would you like to pick it up? [y/n] ", inventory)
+        x = input_request(["y","n"], "Would you like to pick it up? [y/n] ", inventory, current_room)
 
         if x == "y":
             print("You pick up the crowbar. ")
@@ -95,13 +106,14 @@ while gameover == False:
             time.sleep(1)
 
         print("It seems there is nothing else here for you.")
-        x = input_request(["b"], "Enter 'b' to go to the bank. ", inventory)
+        x = input_request(["b"], "Enter 'b' to go to the bank. ", inventory, current_room)
 
     if x == "b":
         print('\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n')
+        current_room = "outside bank"
         print("You have arrived at the front of your local bank.")
         time.sleep(1)
-        x = input_request(["y","n"], "Would you like to break into the bank? [y/n] ", inventory)
+        x = input_request(["y","n"], "Would you like to break into the bank? [y/n] ", inventory, current_room)
 
         if x == "y":
             print("How would you like to enter?")
@@ -110,12 +122,14 @@ while gameover == False:
 
             if x == "v":
                 print('\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n')
+                current_room = "safe"
                 print("You have made it into the safe.")
+
                 time.sleep(2)
                 print("Which of the following items would you like to use?")
                 time.sleep(1)
                 print("A jackhammer, crowbar or an apple.")
-                x = input_request(["jackhammer", "crowbar", "apple"], "Enter the item you would like to use: ", inventory)
+                x = input_request(["jackhammer", "crowbar", "apple"], "Enter the item you would like to use: ", inventory, current_room)
 
                 if x.lower() in ["crowbar", "jackhammer"]:
                     if x in inventory:
@@ -133,6 +147,7 @@ while gameover == False:
                     print("GAME OVER")
 
             elif x == 'h':
+                current_room = "outside bank"
                 x = input('do you know what you are doing? ')
                 y = input('is your laptop on? ')
                 w = input('do you own the bank? ')
@@ -155,6 +170,7 @@ while gameover == False:
                     print("GAME OVER")
 
             elif x == 'w':
+                current_room = "bank lobby"
                 x = input('do you bank here? ')
                 y = input('are you wearing a mask? ')
                 if x == "yes" and y == "no":
@@ -165,12 +181,13 @@ while gameover == False:
                     print("GAME OVER")
                 else:
                     print('\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n')
+                    current_room = "safe"
                     print("you have made it into the safe.")
                     time.sleep(2)
                     print("Which of the following items would you like to use?")
                     time.sleep(1)
                     print("A jackhammer, crowbar or an apple.")
-                    x = input_request(["jackhammer", "crowbar", "apple"], "Enter the item you would like to use: ", inventory)
+                    x = input_request(["jackhammer", "crowbar", "apple"], "Enter the item you would like to use: ", inventory, current_room)
 
                     if x.lower() in ["crowbar", "jackhammer"]:
                         if x in inventory:
@@ -190,7 +207,7 @@ while gameover == False:
         elif x == "n":
             print("Then let's go to the railroad. ")
             time.sleep(2)
-            x = input_request(["r"], "Enter 'r' to go to the railroad. ", inventory)
+            x = input_request(["r"], "Enter 'r' to go to the railroad. ", inventory, current_room)
 
 ###########
 input("Press enter to end the script. ")
