417226edfeef880ff071ee5f518ef45572de9ed0
mariotosdig <mariotosdig2@gmail.com>
2019-12-11 22:36:14 -0400

    you are, my fire

--- exam_practice5.py
+++ exam_practice5.py
@@ -2,18 +2,41 @@ from random import randint
 
 number = randint(1,100)
 correct = False
+binaryForSomeFuckingReason = False
 
 okBoomer = input('u wanna guess ur number in binary for some reason? ').lower()
 if okBoomer in ['y', 'yes']:
-    pass
+    print('fuck off mate. why u gotta make this hard')
+    binaryForSomeFuckingReason = True
 elif okBoomer in ['n', 'no']:
-    pass
+    print('thank fuck')
 else:
     print('defaulting to integers cuz i dont have time for ur shit. ')
 
 for i in range(3):
     if correct == False:
         user_input = int(input("bro what the number be lmao. "))
+        if binaryForSomeFuckingReason == True:
+            ## convert that shit:
+            if True:
+                pass
+            if True:
+                pass
+            if True:
+                pass
+            if True:
+                pass
+            if True:
+                pass
+            if True:
+                pass
+            if True:
+                pass
+            if True:
+                pass
+
+
+            pass
         if number == user_input:
             print('ye')
             correct = True
