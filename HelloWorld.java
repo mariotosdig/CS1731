cf1f2b722d6c08a1d030f0364fae5908a9477e23
mariotosdig <mariotosdig2@gmail.com>
2020-01-13 11:11:18 -0400

    voids to void

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,12 @@
-import java.util;
+import java.util.*;
 public class HelloWorld{
-    public static voids main(String[] args){
+    public static void main(String[] args){
         System.out.println(Math.sqrt(-1) == Math.sqrt(-1));
     }
+}
 
-    class OtherClass{
-        System.out.println("Hello World.");
+class OtherClass{
+    public void name(){
+      System.out.println("Hello World.");
     }
 }
