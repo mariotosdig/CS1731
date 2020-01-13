d2a00a2a63a401991a76f4aaec79e8b9aadf864f
mariotosdig <mariotosdig2@gmail.com>
2020-01-13 10:55:52 -0400

    iiii

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
-public class HelloWorld {
-    public static voids main(String[] args) {
-        System.out.println(Math.sqrt(-1) Math.sqrt(-1));
+public class HelloWorld{
+    public static voids main(String[] args){
+        System.out.println(Math.sqrt(-1) == Math.sqrt(-1));
+    }
+
+    class OtherClass{
+        System.out.println("Hello World.");
     }
 }
