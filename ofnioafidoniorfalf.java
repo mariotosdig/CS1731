22bab6489a7a676712812be3b0e96ebaa45245bb
mariotosdig <mariotosdig2@gmail.com>
2020-01-20 10:52:31 -0400

    becker jar unzipped lol

---
+++ ofnioafidoniorfalf.java
@@ -0,0 +1,9 @@
+// ofnioafidoniorfalf
+
+import becker.robots.*;
+public class ofnioafidoniorfalf{
+    public static void main(String[] args){
+      City bruh = new City(1,1);
+      Robot r1 = new Robot(bruh, 5, 1, Direction.SOUTH, 99);
+    }
+}
