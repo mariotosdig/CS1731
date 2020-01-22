eaea08ff1174da29a36d2b784577cd6e9bc51a1d
mariotosdig <mariotosdig2@gmail.com>
2020-01-22 13:22:08 -0400

    srtyhbnmloiuytrfvbvcxserthjko9uyhb

--- ofnioafidoniorfalf.java
+++ ofnioafidoniorfalf.java
@@ -5,6 +5,8 @@ public class ofnioafidoniorfalf{
     public static void main(String[] args){
       City bruh = new City(15,25);
       Robot r1 = new Robot(bruh, 5, 1, Direction.SOUTH);
-      r1.move();
-    }
-}
+      while(true){
+        r1.move();
+      }
+    } //main
+} //not main
