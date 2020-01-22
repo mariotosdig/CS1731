584c5874769cdf6ceb91b059c2d9f4a0bc5b725d
mariotosdig <mariotosdig2@gmail.com>
2020-01-22 10:48:48 -0400

    idihfqoiewphfqhfhfih

--- ofnioafidoniorfalf.java
+++ ofnioafidoniorfalf.java
@@ -3,7 +3,8 @@
 import becker.robots.*;
 public class ofnioafidoniorfalf{
     public static void main(String[] args){
-      City bruh = new City(1,1);
-      Robot r1 = new Robot(bruh, 5, 1, Direction.SOUTH, 99);
+      City bruh = new City(15,25);
+      Robot r1 = new Robot(bruh, 5, 1, Direction.SOUTH);
+      r1.move();
     }
 }
