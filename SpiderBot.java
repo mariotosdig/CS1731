48aa57409ac3034ec224001d34f21c600cfea0b7
mariotosdig <mariotosdig2@gmail.com>
2020-02-06 16:45:14 -0400

    created bot file

---
+++ SpiderBot.java
@@ -0,0 +1,9 @@
+import becker.robots.*;
+public class SpiderBot extends Robot{
+    public SpiderBot(City city, int ave, int str, Direction dir){
+      super(city, ave, str, dir);
+    }
+    public void crawlWalls(){
+      do stuff idk
+    }
+}
