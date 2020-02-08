3a5da60a3ee105d48e0353281655002c7f26b921
mariotosdig <mariotosdig2@gmail.com>
2020-02-08 15:30:38 -0400

    java 8 panel init

---
+++ Streams.java
@@ -0,0 +1,7 @@
+import java.util.stream.*;
+public class Streams{
+    public static void main(String[] args){
+        Stream stream = Stream.of(7,2,4,129,16,15);
+        System.out.println(stream);
+    }
+}
