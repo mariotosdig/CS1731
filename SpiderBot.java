410109ffbd7a00aba71a70ca6ed742748b05b28a
mariotosdig <mariotosdig2@gmail.com>
2020-02-10 18:53:47 -0400

    comment at top

--- SpiderBot.java
+++ SpiderBot.java
@@ -1,3 +1,8 @@
+// COMP 1731
+// Assignment 1
+// Andrew MacKenzie
+// 02/11/2020
+
 import becker.robots.*;
 public class SpiderBot extends Robot{
     public SpiderBot(City city, int ave, int str, Direction dir){
